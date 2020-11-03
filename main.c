99b6d3b21cfffe3abea31a707e368a03a761ab37
Gayuh Tri Rahutami <69070763+wundersmith@users.noreply.github.com>
2020-11-03 21:56:02 +0700

    changed player.h and main.c

--- main.c
+++ main.c
@@ -12,38 +12,14 @@ char CC;
 boolean EOP;
 MATRIKS Peta1, Peta2, Peta3, Peta4;
 
-int loadPeta(){
-    FILE *cFile;
-    char* filename;
-    char str[MAXCHAR];
-    int i;
-
-    // start of peta 1
-    
-    MakePETA("Peta/Peta_1.txt", &Peta1);
-
-    // end of peta 1
-
-    // start of peta 2
-
-    MakePETA("Peta/Peta_2.txt", &Peta2);
-
-    // end of peta 2
-
-    // start of peta 3
-
-    MakePETA("Peta/Peta_3.txt", &Peta3);
-
-    // end of peta 3
-
-    // start of peta 4
-
-    MakePETA("Peta/Peta_4.txt", &Peta4);
-
-    // end of peta 4
+void Preparation(){
 }
 
-int main(){
+void MainPhase(){
+
+}
+
+int MainMenu(){
     int menu;
     printf("====================================================================\n\n");
     printf("                Welcome to Willy Wangky's World!\n");
@@ -54,50 +30,66 @@ int main(){
     scanf("%d", &menu);
 
     printf("====================================================================\n");
+    return menu;
+}
 
-    if (menu == 1){
-
-        player p1;
-        POINT p;
-        char charInput;
-        char name[20];
-
-        p.X = 1;
-        p.Y = 1;
-
-        printf("Enter your name: ");
-        scanf("%s", &name);
-
-        CreatePlayer(&p1, p, name);
-
-        printf("====================================================================\n");
-        loadPeta();
-
-        TulisPETA(Peta1, p1);
-
-        TulisPlayer(p1);
-        printf("====================================================================\n");
-        scanf(" %c", &charInput);
-
-        while (charInput != '0'){
-            if (charInput == 'w'){
-                w(&p1, Peta1);
+void MenuJalan(player *p1, char charInput){
+    if (charInput == 'w'){
+                w(p1, Peta1);
             }
             else if (charInput == 'a'){
-                a(&p1, Peta1);
+                a(p1, Peta1);
             }
             else if (charInput == 's'){
-                s(&p1, Peta1);
+                s(p1, Peta1);
             }
             else if (charInput == 'd'){
-                d(&p1, Peta1);
+                d(p1, Peta1);
             }
 
             printf("=========================================\n");
-            TulisPETA(Peta1, p1);
+            TulisPETA(Peta1, *p1);
             printf("\n=========================================\n");
-            TulisPlayer(p1);
+            TulisPlayer(*p1);
             printf("=========================================\n");
+            scanf(" %c", &charInput);
+}
+
+void NewGame(player *p1){
+    POINT p;
+    char name[20];
+
+    p.X = 1;
+    p.Y = 1;
+
+    printf("Enter your name: ");
+    scanf("%s", &name);
+
+    CreatePlayer(&p1, p, name);
+
+    printf("====================================================================\n");
+    loadPeta();
+
+    TulisPETA(Peta1, p1);
+
+    TulisPlayer(*p1);
+    printf("====================================================================\n");
+
+}
+
+int main(){
+    int menu;
+    
+    menu = MainMenu();
+
+    if (menu == 1){
+        player p1;
+        char charInput;
+        
+        NewGame(&p1);
+
+        while (charInput != '0'){
+            MenuJalan(&p1, charInput);
             scanf(" %c", &charInput);
         }
 
